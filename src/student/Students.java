4075714fbae6072329f337a5f0b6774bfd17dc78
Divya <srinivad@sheridancollege.ca>
2022-01-25 12:57:22 -0500

    Added address variable in students.java file

--- src/student/Students.java
+++ src/student/Students.java
@@ -4,12 +4,21 @@ package student;
 public class Students {
     private String studentId;
     private String studentName;
+    private String address;
 
 
 public Students(String studentId, String studentName){
      this.studentId = studentId;
      this.studentName = studentName;
      }   
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getStudentId() {
         return studentId;
